5a2e72d1a4e6e03a9641eed6fd1371b34743e213
Jeremy <jdbranham@hotmail.com>
2017-09-25 17:46:03 -0500

    updates

--- src/sprout-plugin-loader.component.ts
+++ src/sprout-plugin-loader.component.ts
@@ -1,4 +1,6 @@
-import { Component, NgModuleFactoryLoader, SystemJsNgModuleLoader, Injector, AfterViewInit, Compiler } from '@angular/core';
+import { Component, NgModuleFactoryLoader, SystemJsNgModuleLoader, AfterViewInit } from '@angular/core';
+import { SproutPluginRegistryService } from './sprout-plugin-registry.service';
+
 
 @Component({
   template: `<h1>hello from the plugin loader`,
@@ -11,19 +13,12 @@ import { Component, NgModuleFactoryLoader, SystemJsNgModuleLoader, Injector, Aft
 })
 export class SproutPluginLoaderComponent implements AfterViewInit {
 
-  constructor(private _injector: Injector,
-              private _compiler: Compiler) {
+  constructor(
+              private _pluginRegistryService: SproutPluginRegistryService) {
+    this._pluginRegistryService.getPlugins();
   }
 
   ngAfterViewInit() {
-      System.import('http://localhost:8080/sprout-wiki-plugin.bundle.js').then((module) => {
-      this._compiler.compileModuleAndAllComponentsAsync(module.TModule)
-        .then((compiled) => {
-          const m = compiled.ngModuleFactory.create(this._injector);
-          const factory = compiled.componentFactories[0];
-          const cmp = factory.create(this._injector, [], null, m);
-          console.log(cmp);
-        })
-    })
+
   }
 }
